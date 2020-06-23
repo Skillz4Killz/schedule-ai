6582919729a5b41e1a94e8c1176ba5d991aa9880
Skillz <gankstarsleague@gmail.com>
2020-06-23 12:04:50 -0400

    base of bot

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { KlasaClient } from "klasa";
+import config from "./config";
+import { Intents } from "@klasa/ws";
+
+const client = new KlasaClient({
+  commands: {
+    prefix: ".",
+    logging: true,
+    editing: true,
+    messageLifetime: 600000,
+  },
+  rest: {
+    offset: 0,
+  },
+  consoleEvents: {
+    debug: true,
+  },
+  cache: {
+    messageLifetime: 300000,
+    messageSweepInterval: 60000,
+  },
+  ws: {
+    intents: Intents.FLAGS.GUILD_MESSAGES | Intents.FLAGS.GUILDS,
+  },
+});
+
+client.token = config.token;
+
+client.connect();
